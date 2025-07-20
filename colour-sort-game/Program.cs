bbeb46971f7195367043768664144cd0d2275184
Asher Paes <asherpaes09@gmail.com>
2025-07-20 17:41:41 +0100

    Add project files.

---
+++ colour-sort-game/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace colour_sort_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = 5;
+
+            Stack<int> origin = new Stack<int>();
+            origin.Push(0);
+            origin.Push(0);
+            origin.Push(1);
+            origin.Push(2);
+            origin.Push(2);
+            Stack<int> final = new Stack<int>();
+            final.Push(0);
+            final.Push(1);
+            final.Push(2);
+            final.Push(2);
+
+            int ItemToMove = CheckMove(origin, final, height);
+            if (ItemToMove != -1)
+            {
+                MakeMove(origin, final, height, ItemToMove);
+            }
+
+            foreach (var item in origin)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            System.Console.WriteLine("Final");
+            foreach (var item in final)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+        }
+        static int CheckMove(Stack<int> originStack, Stack<int> finalStack, int height)
+        {
+            // Colours need to be the same between origin and final DONE
+            // FinalStack must have space (can't have a height of over the height variable) DONE
+            // If there are multiple of the same colour in a row in origin, and space for them in final, then those multiple colours move together
+
+
+            int TopItemOrigin = originStack.Peek();
+            int TopItemFinal = finalStack.Peek();
+
+            if (finalStack.Count >= height)
+            {
+                return -1;
+            }
+
+            if (TopItemOrigin != TopItemFinal)
+            {
+                return -1;
+            }
+            return TopItemFinal;
+
+        }
+
+        static void MakeMove(Stack<int> originStack, Stack<int> finalStack, int height, int ItemToMove)
+        {
+            while (originStack.Peek() == ItemToMove && finalStack.Count < height)
+            {
+                int item = originStack.Pop();
+                finalStack.Push(item);
+            }
+
+        }
+    }
+}
---
+++ colour-sort-game/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("colour-sort-game")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Razer")]
+[assembly: AssemblyProduct("colour-sort-game")]
+[assembly: AssemblyCopyright("Copyright © Razer 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1535b5c4-168f-4809-b814-68d77782633e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
